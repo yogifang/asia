cef2640449ae419b442c1d74cefb27eec7d0e4ea
fantzulin <fanchi0917@gmail.com>
2021-06-13 21:45:58 +0800

    修改配色, 修改儲存按鈕的位置 bug, 修改上方 Tab 的 bug, 還需要改背景圖

--- client/src/forms/fmBasicInfo.js
+++ client/src/forms/fmBasicInfo.js
@@ -189,8 +189,8 @@ export default function BasicInfoForm() {
       <Box pl={100} />
       <Grid container>
         <div className='div-scroll'>
+          <Button className='btn-save' variant='contained' color='primary' onClick={handleClick} style={{ backgroundImage: "url(" + saveImage + ")" }}></Button>
           <div className='row'>
-            <Button className='btn-save' variant='contained' color='primary' onClick={handleClick} style={{ backgroundImage: "url("+ saveImage +")"}}></Button>
             <div className='col s12'>
               <p className='center-align text-orange'>若以下資料有不便回答者 可填入N</p>
             </div>
--- client/src/forms/fmConfirm.js
+++ client/src/forms/fmConfirm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Grid, Typography, Button } from '@material-ui/core';
 import Controls from '../components/controls/Controls';
 import { useForm, Form } from '../components/useForm';
-import Status from './status' ;
+import Status from './status';
 
 const confirmItems = [
   { id: 'yap', title: '確定' },
@@ -11,15 +11,15 @@ const confirmItems = [
 
 const initialFValues = {
   id: 0,
-  confirm: 'nope' ,
- 
+  confirm: 'nope',
+
 
 };
 
 export default function BasicInfoForm() {
   const validate = (fieldValues = values) => {
     let temp = { ...errors };
-    
+
     setErrors({
       ...temp,
     });
@@ -47,28 +47,30 @@ export default function BasicInfoForm() {
   return (
     <Form onSubmit={handleSubmit}>
       <Grid container >
-        <div className="row">
-          <div className="col s12 center-align">
-            <p className="text-purple">
-              本人確定以上所有資訊皆據實填寫<br />
-              I hereby declare that the above information is true and accurate to the best of my knowledge.
-            </p>
-            <label>
-              <input name="group1" type="radio" />
-              <span>確定</span>
-              <span>YES</span>
-            </label>
-            <div style={{'margin-top': "100px"}}>
-              <Button variant='contained' color='primary'>
-                送出
-              </Button>
-            </div>
-            <div className="d-inline-block boder-orange mt-100 p-1">
-              <p className="text-orange">
-                如有任何疑問<br />
-                請聯絡 support@findyourathlete.com<br />
-                謝謝！
+        <div className='div-scroll'>
+          <div className="row">
+            <div className="col s12 center-align">
+              <p className="text-purple">
+                本人確定以上所有資訊皆據實填寫<br />
+                I hereby declare that the above information is true and accurate to the best of my knowledge.
               </p>
+              <label>
+                <input name="group1" type="radio" />
+                <span>確定</span>
+                <span>YES</span>
+              </label>
+              <div style={{ 'margin': "50px auto" }}>
+                <Button variant='contained' color='primary'>
+                  送出
+                </Button>
+              </div>
+              <div className="d-inline-block boder-orange p-1">
+                <p className="text-orange">
+                  如有任何疑問<br />
+                  請聯絡 support@findyourathlete.com<br />
+                  謝謝！
+                </p>
+              </div>
             </div>
           </div>
         </div>
